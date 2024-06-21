252851d4220dc744a5adb459ea436ec24f06336c
Sumit Pandey <spandey247789@gmail.com>
2024-06-21 10:11:45 +0530

    setting up components

--- app/page.js
+++ app/page.js
@@ -8,18 +8,23 @@ import {
   Insights,
   Feedback,
 } from "@/sections";
+import { Navbar, Footer } from "@/components";
 
 export default function Home() {
   return (
-    <main>
-      <Hero />
-      <About />
-      <Explore />
-      <GetStarted />
-      <WhatsNew />
-      <World />
-      <Insights />
-      <Feedback />
-    </main>
+    <>
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Explore />
+        <GetStarted />
+        <WhatsNew />
+        <World />
+        <Insights />
+        <Feedback />
+      </main>
+      <Footer />
+    </>
   );
 }
